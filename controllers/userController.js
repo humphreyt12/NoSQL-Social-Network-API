6184c3a251bad8137afa7de99df1760dc11c11ab
Tina Humphrey <humphrey14t@gmail.com>
2024-03-06 23:58:22 -0500

    modified userroutes.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,6 @@ module.exports = {
         if (!user) {
           return res.status(404).json({ message: 'No user with that ID' });
         }
-    
         res.json(user);
       } catch (err) {
         res.status(500).json(err);
